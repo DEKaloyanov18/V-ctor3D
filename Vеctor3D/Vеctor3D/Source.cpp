f3b10a2cfce8c16cdbc24e6f6a9e354d15a2e868
Mitko <58166494+DEKaloyanov18@users.noreply.github.com>
2022-06-09 10:26:40 +0300

    Update Source.cpp

--- Vеctor3D/Vеctor3D/Source.cpp
+++ Vеctor3D/Vеctor3D/Source.cpp
@@ -6,7 +6,11 @@ using namespace std;
 
 
 
+
 class Vector3d {
+
+
+
 private:
 	float x;
 	float y;
@@ -15,50 +19,68 @@ private:
 
 
 public:
-	void operator+=(const Vector3d& v);
 	void operator-=(const Vector3d& v);
+	void operator+=(const Vector3d& v);
 	void operator*=(const Vector3d& v);
 	void operator/=(const Vector3d& v);
+	float calcScalarProduct(const Vector3d& v) const;
+	void print();
 };
 
 
-void Vector3d::operator +=(const Vector3d& v) {
 
 
-
-	x += v.x;
-	y += v.y;
-	z += v.z;
-}
-
-
-void Vector3d::operator -=(const Vector3d& v)
+void Vector3d::operator -=(const Vector3d& subtract)
 {
-	x -= v.x;
-	y -= v.y;
-	z -= v.z;
+	x -= subtract.x;
+	y -= subtract.y;
+	z -= subtract.z;
 }
 
 
 
-void Vector3d::operator *=(const Vector3d& v) {
+void Vector3d::operator +=(const Vector3d& addition) {
 
 
 
-	x *= v.x;
-	y *= v.y;
-	z *= v.z;
+	x += addition.x;
+	y += addition.y;
+	z += addition.z;
 }
 
 
 
-void Vector3d::operator /=(const Vector3d& v) {
+void Vector3d::operator *=(const Vector3d& multiplication) {
 
 
 
-	x /= v.x;
-	y /= v.y;
-	z /= v.z;
+	x *= multiplication.x;
+	y *= multiplication.y;
+	z *= multiplication.z;
+}
+
+
+
+void Vector3d::operator /=(const Vector3d& division) {
+
+
+
+	x /= division.x;
+	y /= division.y;
+	z /= division.z;
+}
+
+
+
+float Vector3d::calcScalarProduct(const Vector3d& v) const {
+	return x * v.x + y * v.y + z * v.z;
+}
+
+
+
+void Vector3d::print()
+{
+	cout << "x = " << this->x << ", y = " << this->y << ", z = " << this->z << endl;
 }
 
 
@@ -67,6 +89,21 @@ int main() {
 
 
 
+	Vector3d vec1;
+	Vector3d vec2;
 
 
+
+	Vector3d result;
+
+
+
+	vec1 += vec2;
+	vec1 *= vec2;
+	vec1 /= vec2;
+	vec1 -= vec2;
+
+
+
+	result.print();
 }
